96255ea012e8b569205ee6aa91859e5f6f5d14a3
Brad Edwards <j.bradley.edwards@gmail.com>
2024-11-23 13:25:33 -0800

    Add basic init and capture

---
+++ src/ingestion/PacketFilter.py
@@ -0,0 +1,12 @@
+# PacketFilter.py
+
+
+class PacketFilter:
+    def __init__(self):
+        raise NotImplementedError
+
+    def filter(self):
+        raise NotImplementedError
+
+    def get_name(self):
+        raise NotImplementedError
